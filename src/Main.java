cc892654581f8ef193b4c7b71ef8ec390b478e89
Kevin Santos <kevinlich180@gmail.com>
2025-03-18 17:45:43 -0500

    ejercicio POO

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,123 +7,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("auto 1");
+        chevrolet();
+        System.out.println("\nauto 2");
+        Kia();
+    }
+    public static void  chevrolet(){
+        Autos chevroletX = new Autos();
+        chevroletX.setColor("rojo");
+        chevroletX.setKilometraje(1200);
+        chevroletX.setModelo("2012");
+        chevroletX.setMarca("chevrolet");
+        chevroletX.setPlaca("FGH523");
 
-      /*  Scanner teclado =new Scanner(System.in);
-        int num;
-        int min= 1;
-        int max =100;
-        Random ale = new Random();
-        int numAle = ale.nextInt(max - min + 1) + min;
-        System.out.println(numAle);
-            System.out.println("ingrese un número entre el uno y el 100");
-            num = teclado.nextInt();
-        do {
+        System.out.println("marca :"+chevroletX.getMarca());
+        System.out.println("color : "+chevroletX.getColor());
+        System.out.println("Kilometraje :"+chevroletX.getKilometraje());
+        System.out.println("modelo :"+chevroletX.getModelo());
+        System.out.println("placa :"+chevroletX.getPlaca());
+    }
 
-                //System.out.println("--------Winner----------");
- if (numAle > num) {
-                System.out.println("El número es mayor que el ingresado ");
-                System.out.println("por favor ingrese nuevamente un numero");
-                num = teclado.nextInt();
+    public static void  Kia(){
+        Autos kiaX = new Autos();
+        kiaX.setColor("Blanco");
+        kiaX.setKilometraje(1350.5);
+        kiaX.setModelo("2013");
+        kiaX.setMarca("Renault");
+        kiaX.setMarca("GDF784");
 
-            } else if (num > numAle) {
-                System.out.println("El numero es menor que el ingresado");
-                System.out.println("por favor ingrese nuevamente un numero");
-                num = teclado.nextInt();
-            }
-        }while(num != numAle);
-        System.out.println("--------Winner----------");*/
-
-// numeros pares
-        /*
-        for(int i=1;i<101;i++){
-            if(i%2==0){
-                System.out.println(i+"\t El número es par ");
-            }else{
-                System.out.println(i+"\t El número es impar ");
-            }
-
-        }*/
-
-
-        // número primo
-
-
-   /*     for (int i=1;i<=4;i++){
-            int cont=0;
-        for (int j = 1; j <= i; j++) {
-            if (i % j == 0) {
-                cont++;
-            }
-
-
-        }
-            if (cont > 2) {
-                System.out.println(i+"el numero no es primo");
-            } else {
-                System.out.println(i+"el número es primo");
-            }
-
-
-        }*/
-        //practica
-       /* Scanner teclado=new Scanner(System.in);
-        boolean estado=true;
-        do {
-            int cont = 0;
-            System.out.println("ingrese un número para validar si es primo");
-            int numero = teclado.nextInt();
-
-            for (int i = 1; i < numero; i++) {
-                if (numero % i == 0) {
-                    cont++;
-                }
-            }
-
-            if (cont > 2) {
-                System.out.println("no es número primo");
-            } else {
-                System.out.println("es numero primo");
-            }
-        }while (estado);*/
-/*int min=1;
-int max=6;
-int minL=1;
-int maxL=10;
-int a=0;
-int b=0;
-Random ale= new Random();
-int lanzadas =ale.nextInt(maxL-minL+1)+minL;
-for(int i=1;i<lanzadas;i++){
-        int dado1 =ale.nextInt(max-min+1)+min;
-         int dado2 =ale.nextInt(max-min+1)+min;
-    System.out.println("lanzamiento\t"+i+"\tdado1 "+dado1+"\tdado2 "+dado2);
-    a+=dado1;
-    b+=dado2;
-        }
-        System.out.println("resultados\tdado1 "+a+"\tdado2 "+b);
-if(a>b){
-    System.out.println("el ganador es el dado 1");
-}else{
-    System.out.println("el ganador es el dado 2");
-}*/
-
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("operaciones");
-        System.out.println("a.suma ");
-        System.out.println("b. resta");
-        System.out.println("c. multiplicacion");
-        System.out.println("d. division ");
-        String opc = teclado.nextLine();
-
-        double resultado = switch (opc) {
-            case "a" -> 1 + 1;
-            case "b" -> 2 - 1;
-            case "c" -> 2 * 2;
-            case "d" -> 2 / 2;
-            default -> 2*8;
-        };
-        System.out.println("el resultado es"+resultado);
+        System.out.println("marca :"+kiaX.getMarca());
+        System.out.println("color : "+kiaX.getColor());
+        System.out.println("Kilometraje :"+kiaX.getKilometraje());
+        System.out.println("modelo :"+kiaX.getModelo());
+        System.out.println("placa :"+kiaX.getPlaca());
     }
 }
 
