6c8812595bdcbcb9c4b81146c3f80cc3c918fb4a
Kevin Santos <kevinlich180@gmail.com>
2025-04-02 19:39:29 -0500

    UML

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
-
-        int n1, n2;
-        double raiz1, raiz2;
-
-        do {
-            System.out.println("ingrese un valor de n1");
-            n1 = teclado.nextInt();
-        } while (n1 <= 0);
-
-        do {
-            System.out.println("ingrese un valor de n2");
-            n2 = teclado.nextInt();
-        } while (n2 <= 0);
-
-        raiz1 = Math.sqrt(n1);
-        raiz2 = Math.sqrt(n2);
-
-        System.out.println("la raiz de na es" + raiz1);
-        System.out.println("la raiz de na es" + raiz2);
 
 
     }
